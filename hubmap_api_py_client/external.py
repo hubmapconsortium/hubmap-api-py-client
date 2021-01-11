51bdcec4233515ec17a40eef4a82771c567cf940
mccalluc <mccalluc@users.noreply.github.com>
2021-01-11 17:36:26 -0500

    start offset

--- hubmap_api_py_client/external.py
+++ hubmap_api_py_client/external.py
@@ -1,7 +1,6 @@
 from hubmap_api_py_client.internal import InternalClient
 
 _default_limit = 1000
-_default_p_value = -1
 
 
 class ExternalClient():
@@ -32,7 +31,7 @@ def _add_method(output_type, ResultsSetSubclass):
     method_name = f'select_{output_type}s'
     method = (
         lambda self, where=None, has=None,
-        genomic_modality=None, limit=_default_limit, p_value=_default_p_value:
+        genomic_modality=None, limit=_default_limit, p_value=None:
         self._query(
             input_type=where, output_type=output_type, has=has,
             genomic_modality=genomic_modality, limit=limit, p_value=p_value,
@@ -80,14 +79,15 @@ class ResultsSet():
             input_type=self.input_type, output_type=self.output_type,
             query=self.query)
 
-    def get_list(self, limit):
-        return self.client.set_list_evaluation(self.handle, self.output_type, limit)
+    def get_list(self, limit, offset=0):
+        return self.client.set_list_evaluation(self.handle, self.output_type, limit, offset=offset)
 
     def get_details(
-            self, limit,
+            self, limit, offset=0,
             values_included=[], sort_by=None):
         return self.client.set_detail_evaluation(
             self.handle, self.output_type, limit,
+            offset=offset,
             sort_by=sort_by,
             values_type=self.input_type,
             values_included=[self.query])
